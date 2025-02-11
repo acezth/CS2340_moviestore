93be7c9a3eae7072be3983452ac0b532e05ef881
Dmitri-Morris <Dmitri.Morris@protonmail.com>
2025-02-10 20:55:51 -0500

    Ended on Page 65 of the textbook, "Accessing the Django admin interface."

--- moviesstore/moviesstore/settings.py
+++ moviesstore/moviesstore/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'movies',
 ]
 
 MIDDLEWARE = [
@@ -125,5 +126,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'moviestore/static/',
+ BASE_DIR / 'moviesstore/static/',
 ]
