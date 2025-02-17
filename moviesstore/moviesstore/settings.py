47617894175efdf25517c41364df3c7ac6c22bbe
carl <carlpaolino05@gmail.com>
2025-02-17 15:43:39 -0500

    tried to make more hosts

--- moviesstore/moviesstore/settings.py
+++ moviesstore/moviesstore/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-_rc04*(yb2nj%0(trh@zohc84d^s$uvx!y_sg@+g41f^pfa3my
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
